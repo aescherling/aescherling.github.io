5a8ef2e192733d306020415eb6fe5f12f31e3303
Adam Scherling <adamscherling@gmail.com>
2017-09-27 10:54:54 -0700

    Add files via upload

--- pfr/bsf/bsf.js
+++ pfr/bsf/bsf.js
@@ -1,37 +1,47 @@
 // inspiration from https://bl.ocks.org/mbostock/3885304
 
-// Constants for sizing
-var w = window,
-    d = document,
-    e = d.documentElement,
-    g = d.getElementsByTagName('body')[0],
-    x = w.innerWidth || e.clientWidth || g.clientWidth,
-    y = w.innerHeight|| e.clientHeight|| g.clientHeight;
-
-// maintain a ratio of 2.5 width to 1 height, but scale to window. Max width of 1000.
-var width = d3.min([1000, y * 1.2, x * 0.75]);
-var height = width / 2.5;
-
-var svg = d3.select("#vis")
-  .append("svg")
-  .attr('height', height)
-  .attr('width', width);
-
-var graphMargin = {top: 20, right: width * 0.4, bottom: 20, left: 50},
-  graphWidth = width - graphMargin.left - graphMargin.right,
-  graphHeight = height - graphMargin.top - graphMargin.bottom;
-
-var x = d3.scaleBand().rangeRound([0, graphWidth]).padding(0.1),
-    y = d3.scaleLinear().rangeRound([graphHeight, 0]);
-
-var axisTextSize = d3.min([d3.max([8, (graphHeight / 20)]), 15]) + "px"
-
-var graph = svg.append("g")
-    .attr("transform", "translate(" + graphMargin.left + "," + graphMargin.top + ")");
-
-
+// function to run on the data
 plotData = function(error, data, dataType) {
   if (error) throw error;
+
+  /* Initial setup. Then draw axes using percentage scale, fiscal years, draw bars with height 0 */
+
+  // Constants for sizing
+  var w = window,
+      d = document,
+      e = d.documentElement,
+      g = d.getElementsByTagName('body')[0],
+      x = w.innerWidth || e.clientWidth || g.clientWidth,
+      y = w.innerHeight|| e.clientHeight|| g.clientHeight;
+
+  // maintain a ratio of 2.5 width to 1 height, but scale to window. Max width of 1000.
+  var width = d3.min([1000, y * 1.2, x * 0.75]);
+  var height = width / 2.5;
+
+  // scale the toolbars
+  var label_size = d3.min([d3.max([12, (height / 15)]), 18]) + "px",
+      entry_size = d3.min([d3.max([10, (height / 20)]), 16]) + "px",
+      heading_size = d3.min([d3.max([16, (height / 10)]), 46]) + "px";
+  d3.selectAll('#heading').style('font-size', heading_size);
+  d3.selectAll('.toolbar_label').attr('style', "padding:0px 0px 0px 10px; font-weight: bold; font-size: " + label_size);
+  d3.selectAll('.btn-group').selectAll('.btn').attr('style', "font-size: " + entry_size);
+  
+  var svg = d3.select("#vis")
+    .append("svg")
+    .attr('height', height)
+    .attr('width', width);
+
+  var graphMargin = {top: 40, right: width * 0.4, bottom: 20, left: 80},
+    graphWidth = width - graphMargin.left - graphMargin.right,
+    graphHeight = height - graphMargin.top - graphMargin.bottom;
+
+  var x = d3.scaleBand().rangeRound([0, graphWidth]).padding(0.1),
+      y = d3.scaleLinear().rangeRound([graphHeight, 0]);
+
+  var axisTextSize = d3.min([d3.max([8, (graphHeight / 20)]), 15]) + "px"
+
+  var graph = svg.append("g")
+    .attr("transform", "translate(" + graphMargin.left + "," + graphMargin.top + ")");
 
   x.domain(data.map(function(d) { return d.year; }));
   y.domain([0, 0.12]);
@@ -47,20 +57,26 @@ plotData = function(error, data, dataType) {
   graph.append("g")
       .style('font-size', axisTextSize)
       .attr("class", "axis axis--y")
-      .call(d3.axisLeft(y).ticks(6, "%"));
+      .call(d3.axisLeft(y).ticks(6, "%"))
+      .append("text")
+      .classed('yAxisText', true)
+      .attr('x', 10)
+      .attr('y', -30)
+      .attr("dy", "0.71em")
+      .attr("fill", "#000")
+      .text("");
 
-  /* draw bars */
+  /* draw bars with height 0 */
 
-  // total funds (although only Reserve Fund will be visible)
-  // on mouseover, outlines whole bar.
+  // total funds in the background. draw the combined height; only (total - BSF) = reserve will show up.
   graph.selectAll('tBar')
     .data(data)
     .enter().append('rect')
     .attr('class', 'bar tBar')
     .attr('x', function(d) {return x(d.year)})
-    .attr('y', function(d) {return y(d.total_pct)})
+    .attr('y', graphHeight)
     .attr('width', x.bandwidth())
-    .attr('height', function(d) {return graphHeight - y(d.total_pct); })
+    .attr('height', 0)
     .attr('fill', 'skyblue')
     .on('mouseenter', showFloatingTooltip)
     .on('mouseleave', function() {
@@ -68,15 +84,15 @@ plotData = function(error, data, dataType) {
       floating_tooltip.hideTooltip();
     });
 
-  // bar for BSF. on mouseover, outlines whole bar.
+  // bar for BSF
   graph.selectAll(".bsfBar")
     .data(data)
     .enter().append("rect")
       .attr("class", "bar bsfBar")
       .attr("x", function(d) { return x(d.year); })
-      .attr("y", function(d) { return y(d.bsf_pct); })
+      .attr("y", graphHeight)
       .attr("width", x.bandwidth())
-      .attr("height", function(d) { return graphHeight - y(d.bsf_pct); })
+      .attr("height", 0)
       .attr('fill', 'steelblue')
       .on('mouseenter', showFloatingTooltip)
       .on('mouseleave', function() {
@@ -84,7 +100,7 @@ plotData = function(error, data, dataType) {
         floating_tooltip.hideTooltip();
       });
 
-  // make legend
+  // draw legend
   legend = graph.append('g');
   legendX = width * 0.6;
   legendY = graphHeight * 0.25
@@ -118,18 +134,84 @@ plotData = function(error, data, dataType) {
     .attr('font-size', axisTextSize)
     .text('Budget Stabilization Fund');
 
+
+  /* functions for drawing/toggling bars */
+  function drawPercent() {
+    // change y axis (immediate, no transition)
+    var y = d3.scaleLinear().rangeRound([graphHeight, 0]).domain([0, 0.12]);
+    d3.selectAll('.axis--y')
+        .call(d3.axisLeft(y).ticks(6, "%"));
+
+    d3.selectAll('.yAxisText')
+        .text('');
+
+    // transition total funds bars
+    graph.selectAll('.tBar')
+      .transition()
+      .duration(500)
+      .attr('y', function(d) {return y(d.total_pct)})
+      .attr('height', function(d) {return graphHeight - y(d.total_pct); });
+
+    // transition BSF bars
+    // bar for BSF. on mouseover, outlines whole bar.
+    graph.selectAll(".bsfBar")
+      .transition()
+      .duration(500)
+      .attr("y", function(d) { return y(d.bsf_pct); })
+      .attr("height", function(d) { return graphHeight - y(d.bsf_pct); });
+  }
+
+  function drawValues() {
+    // change y axis (immmediate, no transition)
+    var y = d3.scaleLinear().rangeRound([graphHeight, 0]).domain([0, 600]);
+    d3.selectAll('.axis--y')
+        .call(d3.axisLeft(y).ticks(6));
+
+    d3.selectAll('.yAxisText')
+        .text('$ (Millions)');
+
+    // transition total funds bars
+    graph.selectAll('.tBar')
+      .transition()
+      .duration(500)
+      .attr('y', function(d) {return y(d.begin_total / 1e6)})
+      .attr('height', function(d) {return graphHeight - y(d.begin_total / 1e6); });
+
+    // transition BSF bars
+    // bar for BSF. on mouseover, outlines whole bar.
+    graph.selectAll(".bsfBar")
+      .transition()
+      .duration(500)
+      .attr("y", function(d) { return y(d.begin_bsf / 1e6); })
+      .attr("height", function(d) { return graphHeight - y(d.begin_bsf / 1e6); });
+  }
+
+  // default to drawing percentages
+  drawPercent();
+
   /* tooltip for displaying data on each item */
   var floating_tooltip = floatingTooltip('floatingTooltip', "375px");
 
   function showFloatingTooltip(d) {
+    // get the active view
+    view = d3.select('#view_toolbar').selectAll('.btn').filter('.active').attr('id');
+
+    // set the height
+    if (view=='percentages') {
+      var y = d3.scaleLinear().rangeRound([graphHeight, 0]).domain([0, 0.12]);
+      var h = y(d.total_pct);
+    } else if (view=='values') {
+      var y = d3.scaleLinear().rangeRound([graphHeight, 0]).domain([0, 600]);
+      var h = y(d.begin_total / 1e6);
+    }
 
     // outline the bar for that year
     graph.append('rect')
       .attr('class', 'outline')
       .attr('x', x(d.year))
-      .attr('y', y(d.total_pct))
+      .attr('y', h)
       .attr('width', x.bandwidth())
-      .attr('height', graphHeight - y(d.total_pct))
+      .attr('height', graphHeight - h)
       .attr('fill', 'none')
       .attr('stroke', 'black')
       .attr('stroke-width', 2);
@@ -143,6 +225,23 @@ plotData = function(error, data, dataType) {
     // display tooltip
     floating_tooltip.revealTooltip(content, d3.event);
   }
+
+  // Sets up the buttons to allow for toggling between modes
+  d3.selectAll('.btn-group')
+    .selectAll('.btn')
+    .on('click', function() {
+        // set all buttons on the clicked toolbar as inactive, then activate the clicked button
+        var p = d3.select(this.parentNode);
+        p.selectAll('.btn').classed('active', false);
+        d3.select(this).classed('active', true);
+  
+        // toggle
+        if (d3.select(this).attr('id')=='percentages') {
+          drawPercent();
+        } else if (d3.select(this).attr('id')=='values') {
+          drawValues();
+        }
+      });
 
 
 }
