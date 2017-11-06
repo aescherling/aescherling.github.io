990393140dfe52ad98ced9c921958da196412a80
aescherling <adam.scherling@gmail.com>
2017-11-06 10:44:11 -0800

    small changes to EconomyPanel.html and mapLA.js

--- EconomyPanel/mapLA.js
+++ EconomyPanel/mapLA.js
@@ -166,105 +166,6 @@ function map_ready(error, geodata, econdata) {
 
 
 
-// // wait until all the data is loaded before proceeding
-// queue()
-//   .defer(d3.json, 'geodata/council_districts.geojson')
-//   .defer(d3.csv, 'data/2017.04_Population2015.csv')
-//   .await(map_ready)
-
-// function map_ready(error, geodata, econdata) {
-//   if (error) throw error;
-
-//   var features = geodata.features;
-
-
-//   var popByDistrict = {};
-//   econdata.forEach(function(d) { popByDistrict[+d.locality.replace(/\D/g, '')] = +d.density; });
-
-//   //var color = d3.scaleLinear()
-//   //  //.domain([d3.min(Object.values(popByDistrict)), d3.max(Object.values(popByDistrict))])
-//   //  .domain([200000, 300000])
-//   //  .range(['white', 'steelblue']);
-
-//   var color = d3.scalePow()
-//     .exponent(0.5)
-//     .domain([0, 20000])
-//     .range(['white', 'steelblue']);
-
-//   getColor = function (d) {
-//     myColor = color(popByDistrict[d.properties.Council_District.replace(/\D/g, '')]);
-//     return myColor;
-//   }
-
-//   mapLayer.selectAll('path')
-//       .data(features)
-//       .enter().append('path')
-//       .attr('d', path)
-//       .attr('id', function (d) {return d.properties.Council_District.replace(/\s/g, '');})
-//       .attr('label', function (d) {return d.properties.Council_District;})
-//       .attr('councilmember', function (d) {return d.properties.Councilmember;})
-//       .classed('selected', false)
-//       .classed('frozen', false)
-//       .classed('district', true)
-//       .attr('vector-effect', 'non-scaling-stroke')
-//       .style('fill', function (d) {return color(popByDistrict[d.properties.Council_District.replace(/\D/g, '')]);})
-//       .style('stroke', 'white')
-//       .style('cursor', 'pointer')
-//       .on('click', mouseclick)
-//       .on('mouseover', mouseover)
-//       .on('mouseout', mouseout)
-//       .attr('opacity', 0.8);
-
-//   // function for making legends
-//   function makeLegend(x, y, size, colors, labels) {
-//     // make the legend object
-//     legend = map_svg.append('g')
-//       .classed('legend', true);
-
-//     // # of items in the legend
-//     var n = colors.length;
-
-//     var yTmp = y - (n * size * 0.5);
-
-//     // legend title
-//     legend.append('text')
-//       .attr('x', x)
-//       .attr('y', yTmp - size)
-//       .text('Population per');
-//     legend.append('text')
-//       .attr('x', x)
-//       .attr('y', yTmp - size * 0.4)
-//       .text('sq. mile');
-
-//     // loop to place the items
-//     for (var i=0; i<n; i++){
-//       legend.append('rect')
-//         .attr('x', x)
-//         .attr('y', yTmp + size * i)
-//         .attr('width', size)
-//         .attr('height', size)
-//         .attr('fill', d3.rgb(colors[i]))
-//         .attr('stroke', d3.rgb(colors[i]));
-//       legend.append('text')
-//         .attr('x', x + 1.5 * size)
-//         .attr('y', 4 + size/2 + yTmp + size * i)
-//         .attr('text-anchor', 'center')
-//         .attr('style', "font-size: " + d3.min([d3.max([10, (size / 2)]), 16]) + "px")
-//         .text(labels[i]);
-//     }
-//   }
-
-//   //makeLegend(map_svg_width * 0.7, map_svg_height * 0.5, 30, 
-//   //      [color(3e5), color(2.9e5), color(2.8e5), color(2.7e5), color(2.6e5),
-//   //       color(2.5e5), color(2.4e5), color(2.3e5), color(2.2e5), color(2.1e5)], 
-//   //      ['300,000', '290,000', '280,000', '270,000', '260,000', '250,000', '240,000', '230,000', '220,000', '210,000']);
-
-//   makeLegend(map_svg_width * 0.7, map_svg_height * 0.5, 30, 
-//         [color(2e4), color(1e4), color(0.5e4), color(0.25e4), color(0.125e4), color(0.0625e4)],
-//         ['20,000', '10,000','5,000','2,500', '1,250', '625']);
-
-// }
-
 
 
 
@@ -281,27 +182,6 @@ cd_councilmember = map_svg.append('text')
   .attr('text-anchor','left')
   .attr('style', 'font-size: 16px')
   .text('');
-
-  // label council districts
-//  mapLayer.append("g")
-//    .attr("class", "label")
-//    .selectAll("text")
-//    .data(features)
-//    .enter()
-//    .append("text")
-//  	.attr('id', function (d) {return "label" + d.properties.Council_District;})
-//    .text(function(d){
-//      return d.properties.Council_District;
-//    })
-//    .attr("x", function(d){
-//        return path.centroid(d)[0];
-//    })
-//    .attr("y", function(d){
-//        return  path.centroid(d)[1];
-//    })
-//    .attr("text-anchor","middle")
-//    .attr('fill', 'black');
-
 
 /* 
 Helper functions
